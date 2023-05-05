cfc343e7f206e046405617245d037d4b86dc0687
pandeymangg <anshuman.pandey9999@gmail.com>
2023-05-05 09:47:34 +0530

    added dotfiles for xfce

---
+++ fish/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # ~/.config/fish/config.fish
+
+starship init fish | source
+colorscript -r
+end
---
+++ fish/fish/functions/gph.fish
@@ -0,0 +1,4 @@
+function gph --wraps='git push origin HEAD' --description 'alias gph=git push origin HEAD'
+  git push origin HEAD $argv
+        
+end
